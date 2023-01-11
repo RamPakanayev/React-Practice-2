9d23691ae02e8ada9f4ebd890d5fed2b31418c98
RamPakanayev <rampakanayev@gmail.com>
2023-01-11 18:03:10 +0200

    adding props

--- my-app/src/Components/App.jsx
+++ my-app/src/Components/App.jsx
@@ -7,7 +7,39 @@ function App() {
   return (
     <div>
       <Header />
-      <Note /><Note /><Note /><Note />
+
+      <Note
+        title="make my bed"
+        info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        minima molestias eum illo, incidunt nemo, magni delectus sequi placeat
+        quae impedit, laborum quaerat cumque eveniet laboriosam illum nisi
+        laudantium deserunt.
+      "
+      />
+
+      <Note
+        title="learn react"
+        info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        minima molllum nisi
+        laudaerunt.
+      "
+      />
+
+      <Note
+        title="send faxes"
+        info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        minima molestias eum illo,orum quaerat cumque eveniet laboriosam illum nisi
+        laudantium deserunt.
+      "
+      />
+
+      <Note
+        title="pay taxes"
+        info="Lorem ipsum dolor sit amet consectedit, laborum quaerat cumque eveniet laboriosam illum nisi
+        laudantium deserunt.
+      "
+      />
+
       <Footer />
     </div>
   );
--- my-app/src/Components/Note.jsx
+++ my-app/src/Components/Note.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 
-function Note() {
+function Note(props) {
   return (
     <div className="note">
-      <h1>title</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-        minima molestias eum illo, incidunt nemo, magni delectus sequi placeat
-        quae impedit, laborum quaerat cumque eveniet laboriosam illum nisi
-        laudantium deserunt.
-      </p>
+      <h1>{props.title}</h1>
+      <p>{props.info}</p>
     </div>
   );
 }
